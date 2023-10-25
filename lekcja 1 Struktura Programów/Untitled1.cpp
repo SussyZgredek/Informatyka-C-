c9b14fdae48c4e255947acff90e6e7e84b3bfcf9
SussyZgredek <bartek17m12@gmail.com>
2023-10-25 14:08:57 +0200

    C++ pierwsza lekcja

---
+++ lekcja 1 Struktura Programów/Untitled1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	short liczba = 2;
+	float zmiennoprzecinkowa = 3.532;
+	string tekst = "tekst";
+	char znak = 'a';
+	bool znakowy = false;
+	
+	cout << "Hello World" << endl;
+	cout << "Liczna calkowita: " << liczba;
+	cout << endl;
+	
+	string jakos;
+	cout << "Podaj jakis wyraz: ";
+	cin >> jakos;
+	cout << "Wprowadziles wartosc: " << jakos;
+	
+	return 0;
+	
+}
