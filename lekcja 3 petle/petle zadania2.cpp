61e8111307c37a6819c5edb48008384ec6a67bfc
SussyZgredek <bartek17m12@gmail.com>
2023-11-23 14:49:11 +0100

    Zadanie

---
+++ lekcja 3 petle/petle zadania2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+		int wysokosc, szerokosc;
+		cout << "Podaj wysokosc" << endl;
+		cin >> wysokosc;
+		cout << "Podaj szerokosc" << endl;
+		cin >> szerokosc;
+	for(int i=0; i < wysokosc; i++){
+		for(int j=0; j < szerokosc; j++){
+
+			cout << "*";
+			
+		}
+	cout << endl;
+	} 
+}
